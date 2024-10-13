db31dd6962f30f892a4d7acfe7281e9f31a8ca95
Алексей Макаров <raminov.83@mail.ru>
2024-10-13 13:59:12 +0300

    header layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Logo from '../../public/logo.png'
+import BagImg from '../../public/bag 1.svg'
+import SearchImg from '../../public/search-interface-symbol 1.svg'
+import MenuImg from '../../public/menuImg.svg'
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.container}>
+      <header>
+        <div className={styles.logo}>
+          <Image src={Logo} alt="logo" width={48} height={48} />
+          <p className={styles.logo_name}>Planto.</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+        <div className={styles.navigation}>
+          <div className={styles.nav_container}>
+            <button>
+              <Link href='/Home'>Home</Link>
+            </button>
+            <button>
+              <Link href='/Home'>Plants Type</Link>
+            </button>
+            <button>
+              <Link href='/More'>More</Link>
+            </button>
+            <button>
+              <Link href='/Home'>Contact</Link>
+            </button>
+          </div>
+        </div>
+        <div className={styles.side_menu}>
+          <button>
+            <Link href='/Home'><Image src={SearchImg} alt="logo" width={25} height={25} /></Link>
+          </button>
+          <button>
+            <Link href='/Home'><Image src={BagImg} alt="logo" width={25} height={25} /></Link>
+          </button>
+          <button>
+            <Link href='/Home'><Image src={MenuImg} alt="logo" width={25} height={25} /></Link>
+          </button>
+        </div>
+      </header>
     </div>
   );
 }
